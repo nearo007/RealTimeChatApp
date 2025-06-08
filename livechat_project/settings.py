a5d4343e62c89e2daf821cbdecc189186cee80e6
Nearo007 <araujo8166@gmail.com>
2025-06-07 23:58:41 -0300

    fix static path x4

--- livechat_project/settings.py
+++ livechat_project/settings.py
@@ -125,10 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'livechat_app' / 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'livechat_app' / 'static')
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'livechat_app' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
