469481a8a42bf27a4a39b626c82c96fee0a89d6e
Nearo007 <araujo8166@gmail.com>
2025-06-08 01:16:33 -0300

    add custom domain in allowed hosts

--- livechat_project/settings.py
+++ livechat_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com', 'nearochat.my']
 
 # Application definition
 
