e04703fe0a8aae639a03d7852d6be8dd0a0695af
Nearo007 <araujo8166@gmail.com>
2025-06-08 19:29:40 -0300

    fix max length problems

--- livechat_app/models.py
+++ livechat_app/models.py
@@ -3,11 +3,10 @@ from django.utils import timezone
 
 # Create your models here.
 class Room(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
 
 class Message(models.Model):
     value = models.CharField(max_length=255)
     date = models.DateTimeField(default=timezone.now)
-    user = models.CharField(max_length=100)
-    #room_id = models.CharField(max_length=1000)
+    user = models.CharField(max_length=30)
     room = models.ForeignKey(Room, null=False, default=0 , on_delete=models.CASCADE)
