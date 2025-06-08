512dd152828031cf2265982b8634593d41835bfd
Nearo007 <araujo8166@gmail.com>
2025-06-08 17:08:08 -0300

    fix date

--- livechat_app/models.py
+++ livechat_app/models.py
@@ -7,7 +7,7 @@ class Room(models.Model):
 
 class Message(models.Model):
     value = models.CharField(max_length=255)
-    date = models.DateTimeField(default=timezone.now())
+    date = models.DateTimeField(default=timezone.now)
     user = models.CharField(max_length=100)
     #room_id = models.CharField(max_length=1000)
     room = models.ForeignKey(Room, null=False, default=0 , on_delete=models.CASCADE)
