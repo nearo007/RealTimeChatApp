b05d8cd70e44ec568dae97ca860e015561e476d5
Nearo007 <araujo8166@gmail.com>
2025-06-07 21:51:01 -0300

    setting up for deploy

--- livechat_project/livechat_project/settings.py
+++ livechat_project/livechat_project/settings.py
@@ -24,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@qy04fyu!ud&wl@vz4veuy+5163p9n1%52=_y&z(s)od4@g3ml'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 # Application definition
 
